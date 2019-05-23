4674c2ea069c192ea7e5524e4c664513aba6bf96
Zam0411 <mzambrano41165@gmail.com>
2019-05-22 23:23:30 -0700

    updated build

---
+++ client/build/precache-manifest.1e344e17550fb71e034598de472b900f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "c863cfc4fcc449a68d0b",
+    "url": "/static/js/main.0e8eac3e.chunk.js"
+  },
+  {
+    "revision": "ada74aec7b80a19bafaf",
+    "url": "/static/js/2.294484cc.chunk.js"
+  },
+  {
+    "revision": "c863cfc4fcc449a68d0b",
+    "url": "/static/css/main.116a84c2.chunk.css"
+  },
+  {
+    "revision": "cdd83aeaf12063c36c9b8e7ca7f8cb45",
+    "url": "/index.html"
+  }
+];
--- client/build/service-worker.js
+++ client/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.2134e6a43611db5a7b2dd161cbd8123d.js"
+  "/precache-manifest.1e344e17550fb71e034598de472b900f.js"
 );
 
 workbox.clientsClaim();
