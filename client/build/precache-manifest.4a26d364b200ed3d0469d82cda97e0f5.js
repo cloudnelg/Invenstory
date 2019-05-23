48027ee65c107e0ed11cd3e32d56dbfdedf13536
Joshua Lopez-Roque <joshualopezroque@gmail.com>
2019-05-22 18:46:17 -0700

    new changes for npm

---
+++ client/build/precache-manifest.4a26d364b200ed3d0469d82cda97e0f5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "cc36a71c52b1366b09af",
+    "url": "/static/js/main.aa7f8471.chunk.js"
+  },
+  {
+    "revision": "4e4aa9f77f1dceeaa824",
+    "url": "/static/js/2.75b2a13e.chunk.js"
+  },
+  {
+    "revision": "cc36a71c52b1366b09af",
+    "url": "/static/css/main.6ec77c59.chunk.css"
+  },
+  {
+    "revision": "27973e2bdf71cb03af997d1fa20afdbf",
+    "url": "/index.html"
+  }
+];
--- client/build/service-worker.js
+++ client/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.9cf86ce3ae099a6c878f1a30b4e9d139.js"
+  "/precache-manifest.4a26d364b200ed3d0469d82cda97e0f5.js"
 );
 
 workbox.clientsClaim();
